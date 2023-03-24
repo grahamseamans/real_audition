4a077bc794e0a59faaa15a94fe38b24d696d0b4f
Graham Seamans <seamansgraham@gmail.com>
2023-03-23 23:51:06 -0700

    so close to working in one day :(

--- main.py
+++ main.py
@@ -34,21 +34,37 @@ class File:
     def __init__(self, path):
         self.path = path
         data, rate = sf.read(path, dtype="float32")
-        self.data, self.norm_data = data, data
+        self.data = data
+        self.mixing_lufs = 1
         self.sample_rate = rate
         self.lufs = self.get_lufs()
         self.string = self.get_string()
         self.length_secs = len(self.data) / self.sample_rate
+
+        if self.lufs > -14:
+            self.mastering_data = self.normalize_data(-14)
+        else:
+            self.mastering_data = self.data
 
     def get_lufs(self):
         meter = pyln.Meter(self.sample_rate)
         return meter.integrated_loudness(self.data)
 
     def normalize_data(self, new_loudness):
-        self.norm_data = pyln.normalize.loudness(self.data, self.lufs, new_loudness)
+        return pyln.normalize.loudness(self.data, self.lufs, new_loudness)
 
-    def play(self, start_time=0, device=None):
-        to_play = self.norm_data[int(start_time * self.sample_rate) :, :]
+    def play(self, lufs_var, quietest, start_time, device=None):
+        match lufs_var:
+            case "raw":
+                to_play = self.data
+            case "mixing":
+                if quietest != self.mixing_lufs:
+                    self.mixing_data = self.normalize_data(quietest)
+                    self.mixing_lufs = quietest
+                to_play = self.mixing_data
+            case "mastering":
+                to_play = self.mastering_data
+        to_play = to_play[int(start_time * self.sample_rate) :, :]
         to_play = self.fade_in(to_play)
         sd.play(to_play, self.sample_rate, device=device)
 
@@ -87,11 +103,25 @@ class Player:
         tk.Button(self.root, text="play", command=self.play).pack()
         tk.Button(self.root, text="pause", command=self.pause).pack()
         tk.Button(self.root, text="stop", command=self.stop).pack()
+
+        self.lufs_var = tk.StringVar()
+        ra = tk.Radiobutton(
+            self.root, text="raw", variable=self.lufs_var, value="raw"
+        ).pack()
+        rb = tk.Radiobutton(
+            self.root, text="mixing", variable=self.lufs_var, value="mixing"
+        ).pack()
+        rp = tk.Radiobutton(
+            self.root,
+            text="mastering (-14 lufs)",
+            variable=self.lufs_var,
+            value="mastering",
+        ).pack()
+        self.lufs_var.trace_add("write", callback=self.play)
+
         self.current_track = -1
         self.duration = Duration()
-
         self.files = []
-
         self.update_play()
 
     def run(self):
@@ -106,36 +136,24 @@ class Player:
                 pass
         self.update_table()
 
-    def stream_closed(self):
-        try:
-            sd.get_stream()
-            return False
-        except RuntimeError:
-            return True
-        except sd.PortAudioError:
-            return True
-
     def play(self):
         if not self.files:
             return
         if self.current_track == -1:
-            print(self.current_track)
             self.track_select((0,))
         else:
-            self.files[self.current_track].play(start_time=self.duration.duration)
+            quietest = np.min([file.lufs for file in self.files])
+            self.current_track.play(
+                self.lufs_var, quietest, start_time=self.duration.duration
+            )
             self.duration.start(sd.get_stream().time)
 
     def pause(self):
-        if self.stream_closed():
-            return
-        self.duration.update(sd.get_stream().time)
-        self.files[self.current_track].stop_play()
+        self.current_track.stop_play()
 
     def stop(self):
-        if self.stream_closed():
-            return
         self.duration.reset()
-        self.files[self.current_track].stop_play()
+        self.current_track.stop_play()
         self.draw_waveform()
 
     def update_table(self):
@@ -143,9 +161,9 @@ class Player:
         self.table_strings_var.set(names)
 
     def track_select(self, selection):
-        if not selection:
-            return
-        self.current_track = selection[0]
+        # if not selection:
+        #     return
+        self.current_track = self.files[selection[0]]
         self.get_waveform()
         self.draw_waveform()
         self.play()
@@ -153,7 +171,7 @@ class Player:
     def get_waveform(self):
         width = self.waveform.winfo_width()
         height = self.waveform.winfo_height()
-        data = self.files[self.current_track].data
+        data = self.current_track.data
         maxes = np.amax(data, axis=1)
         self.bars = []
         for clump in np.array_split(maxes, width):
@@ -162,28 +180,26 @@ class Player:
     def draw_waveform(self):
         self.waveform.delete("all")
         height = self.waveform.winfo_height()
-        ratio_played = (
-            self.duration.duration / self.files[self.current_track].length_secs
-        )
+        ratio_played = self.duration.duration / self.current_track.length_secs
         cutoff = int(len(self.bars) * ratio_played)
         for i, bar in enumerate(self.bars):
             color = "grey" if i < cutoff else "black"
             self.waveform.create_line(i, height, i, height - bar, fill=color, width=1)
 
     def seek(self, loc):
-        width = self.waveform.winfo_width()
-        time = self.files[self.current_track].length_secs * (loc / width)
+        time = self.current_track.length_secs * (loc / self.waveform.winfo_width())
         self.duration.reset(time)
         self.play()
 
     def update_play(self):
-        update_ms = 100
-        if self.stream_closed():
-            self.root.after(update_ms, self.update_play)
-        else:
+        try:
             self.duration.update(sd.get_stream().time)
             self.draw_waveform()
-            self.root.after(update_ms, self.update_play)
+        except RuntimeError:
+            pass
+        except sd.PortAudioError:
+            pass
+        self.root.after(100, self.update_play)
 
 
 class Duration:
