7510082dc18d3ca169dbb4092566105e49e39c43
Graham Seamans <seamansgraham@gmail.com>
2023-03-25 13:34:38 -0700

    basically working, still kinda jank

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import tkinter.filedialog
 import tkinter as tk
 from pathlib import Path
 from ttkthemes import ThemedTk
+import ttkthemes
 
 """
 toggle animations
 check for clipping and turn down for mixing -
 maybe keep on turning down lufs by 1 until no clipping errors occour?
+but I never set sometihing to a louder lufs than it currently has
+- how can things clip when I turn them down?
 """
 
 
@@ -20,13 +23,13 @@ class GUI:
 
         self.root = ThemedTk(theme="scidpink")
         self.root.title("true audition")
-        self.root.geometry("920x500+290+85")
-        self.padx = 3
-        self.pady = 0
+        self.root.geometry("920x520+290+85")
+        self.padx = (3, 0)
+        self.pady = (3, 0)
 
         top_pane = tk.Frame(self.root)
         tk.Button(top_pane, text="load files", command=self.load_files).pack(
-            side="left", padx=self.padx
+            side="left"
         )
         self.lufs_var = tk.StringVar()
         radio_values = {
@@ -37,18 +40,20 @@ class GUI:
         for text, value in radio_values.items():
             tk.Radiobutton(
                 top_pane, text=text, variable=self.lufs_var, value=value
-            ).pack(side="right", padx=self.padx)
+            ).pack(side="right")
         self.lufs_var.trace_add(
             "write", callback=lambda var, index, mode: self.set_lufs_status()
         )
         self.lufs_var.set("raw")
         self.status = tk.Text(top_pane, height=1, background="light blue")
-        self.status.pack(side="left", padx=self.padx, pady=self.pady)
+        self.status.pack(side="left")
 
         mid_pane = tk.Frame(self.root)
         self.table_strings_var = tk.StringVar(value=[])
         self.lbox = tk.Listbox(
-            mid_pane, listvariable=self.table_strings_var, background="light blue"
+            mid_pane,
+            listvariable=self.table_strings_var,
+            background="light blue",
         )
         self.lbox.pack(fill="both")
         self.lbox.bind(
@@ -85,13 +90,27 @@ class GUI:
 
         self.loading = False
 
-        self.listened_color = "blue"
-        self.unlistened_color = "dark blue"
+        self.listened_color = "grey"
+        self.unlistened_color = "white"
 
-        self.playing_animation = AsciiAnimamtion("☆*。★ﾟ*♪ヾ(☆ゝз・)ノ", 100)
-        self.loading_animaiton = AsciiAnimamtion("loading    (°▽°)   ", 100)
+        self.playing_animation = AsciiAnimamtion("☆*。★ﾟ*♪ヾ(☆ゝз・)ノ    ", 100)
+        self.loading_animaiton = AsciiAnimamtion("loading    (＠_＠)    ", 100)
         self.sitting_animation = AsciiAnimamtion("(◕‿◕)    ", 100)
+        self.clipping_animation = AsciiAnimamtion(
+            "probably clipping    (ﾒ` ﾛ ´)    ", 100
+        )
         self.update_animations()
+
+        self.clipping_text = None
+
+        self.do_padding(self.root)
+
+    def do_padding(self, frame):
+        for child in frame.winfo_children():
+            self.do_padding(child)
+        f_ty = type(frame)
+        if f_ty != ttkthemes.themed_tk.ThemedTk and f_ty != tk.Frame:
+            frame.pack_configure(padx=self.padx, pady=self.pady)
 
     def key_press(self, event):
         if self.player.is_playing():
@@ -103,6 +122,7 @@ class GUI:
         self.player.set_lufs_mode(self.lufs_var.get())
         self.new_waveform()
         self.playhead = 0
+        self.update_waveform()
 
     def space_bar(self, event):
         if self.player.is_playing():
@@ -132,17 +152,24 @@ class GUI:
         self.table_strings_var.set(names)
 
     def remove_track(self, selection):
+        if len(self.player.tracks) == 0:
+            return
         if not selection:
             return
         selection = selection[0]
-        if selection == 0:
+        if len(self.player.tracks) == 1:
             self.player.remove(selection)
             self.waveform.delete("all")
+            self.update_table()
         else:
             self.player.remove(selection)
-            new_track = selection - 1 if selection > 0 else selection
-            self.player.set_current_track(new_track)
-            self.new_waveform()
+            new_track = (
+                selection - 1 if selection == len(self.player.tracks) else selection
+            )
+            self.track_select([new_track])
+            self.update_table()
+            self.lbox.selection_clear(0, "end")
+            self.lbox.selection_set(new_track)
 
     def track_select(self, selection):
         if not selection:
@@ -182,6 +209,7 @@ class GUI:
         self.player.seek_to_ratio(loc / self.waveform.winfo_width())
         if self.player.has_active_track():
             self.update_waveform()
+            self.player.play()
 
     def update_waveform(self):
         player_playhead = int(self.player.ratio_played() * len(self.bars))
@@ -208,7 +236,7 @@ class GUI:
 
     def update_waveform_resize(self):
         if (
-            self.player.current_track != -1
+            self.player.current_track != None
             and self.waveform_width != self.waveform.winfo_width()
         ):
             self.new_waveform()
@@ -228,6 +256,12 @@ class GUI:
             self.status.insert(1.0, self.loading_animaiton.step())
         else:
             self.status.insert(1.0, self.sitting_animation.step())
+        if self.player.clipping:
+            if self.clipping_text:
+                self.waveform.delete(self.clipping_text)
+            self.clipping_text = self.waveform.create_text(
+                0, 0, text=self.clipping_animation.step(), anchor="nw"
+            )
 
         self.root.after(500, self.update_animations)
 
@@ -255,17 +289,17 @@ class AsciiAnimamtion:
 
 class Player:
     def __init__(self):
-        self.current_track = -1
+        self.current_track = None
         self.tracks = []
         self.playhead = 0
         self.current_sample = 0
         self.sample_rate = 0
-        self.quietest = -14
         self.stream = sd.OutputStream()
         self.lufs_mode = "raw"
+        self.clipping = False
 
     def has_active_track(self):
-        return self.current_track != -1
+        return self.current_track != None
 
     def is_playing(self):
         return self.stream.active
@@ -276,18 +310,22 @@ class Player:
         return self.playhead / self.current_track.length_secs
 
     def remove(self, track_num):
-        if self.current_track == -1:
+        if self.current_track == None:
             return
         del self.tracks[track_num]
         if not self.tracks:
-            self.current_track = -1
+            self.current_track = None
+            self.stream.close()
+            self.sample_rate = 0
+        else:
+            self.get_mixing_lufs()
+        self.check_clipped()
 
     def load_samples(self, outdata, frames, time, status):
         data = self.current_track.get_data(
             self.current_sample,
             self.current_sample + frames,
             self.lufs_mode,
-            self.quietest,
         )
         if len(data) == 0:  # song finished
             self.stop()
@@ -302,12 +340,14 @@ class Player:
 
     def set_lufs_mode(self, mode):
         self.lufs_mode = mode
+        if self.tracks:
+            self.check_clipped()
 
     def get_currently_playing_data(self):
-        return self.current_track.get_data(0, -1, self.lufs_mode, self.quietest)
+        return self.current_track.get_data(0, -1, self.lufs_mode)
 
     def seek_to_ratio(self, ratio):
-        if self.current_track == -1:
+        if self.current_track == None:
             return
         self.playhead = self.current_track.length_secs * ratio
         self.current_sample = int(len(self.current_track.data) * ratio)
@@ -318,10 +358,28 @@ class Player:
                 self.tracks.append(Song(path))
             except sf.LibsndfileError:
                 pass
-        self.quietest = np.min([song.lufs for song in self.tracks])
-        if self.current_track == -1 and self.tracks:  # first start
+        if self.tracks:
+            self.get_mixing_lufs()
+        if self.current_track == None and self.tracks:  # first start
             self.set_current_track(0)
             return "first start"
+
+    def get_mixing_lufs(self):
+        mixing_lufs = np.min([song.lufs for song in self.tracks])
+        while self.check_mix_clipped():
+            for track in self.tracks:
+                track.make_mixing_data(mixing_lufs)
+            mixing_lufs -= 1
+        print(f"mixing lufs: {mixing_lufs}")
+
+    def check_mix_clipped(self):
+        return any([track.check_clipped("mixing") for track in self.tracks])
+
+    def check_clipped(self):
+        if self.current_track:
+            self.clipping = self.current_track.check_clipped(self.lufs_mode)
+        else:
+            self.clipping = False
 
     def set_current_track(self, track_num):
         if self.current_track == self.tracks[track_num]:
@@ -329,6 +387,7 @@ class Player:
         elif not self.tracks:
             return "no tracks"
         self.current_track = self.tracks[track_num]
+        self.check_clipped()
         if self.current_track.sample_rate != self.sample_rate:
             self.stream.close()
             self.sample_rate = self.current_track.sample_rate
@@ -341,7 +400,7 @@ class Player:
         return [track.string for track in self.tracks]
 
     def play(self):
-        if self.current_track == -1:
+        if self.current_track == None:
             return
         self.stream.start()
         print(f"playing {self.current_track.string}")
@@ -360,12 +419,12 @@ class Song:
         self.path = path
         data, rate = sf.read(path, dtype="float32")
         self.data = data
-        self.mixing_lufs = 1
         self.sample_rate = rate
         self.lufs = self.get_lufs()
         self.string = self.get_string()
         self.length_secs = len(self.data) / self.sample_rate
         self.mastering_data = self.normalize_data(-14) if self.lufs > -14 else self.data
+        self.mixing_data = data
 
     def get_lufs(self):
         meter = pyln.Meter(self.sample_rate)
@@ -377,22 +436,25 @@ class Song:
     def get_string(self):
         name = Path(self.path).stem
         lufs = "{:.1f}".format(self.lufs)
-        return f"original lufs: {lufs}  | file name: {name}"
+        return f"original lufs: {lufs}  |  file name: {name}"
 
-    def get_data(self, start, end, lufs_mode, quietest):
+    def make_mixing_data(self, lufs):
+        self.mixing_data = self.normalize_data(lufs)
+
+    def check_clipped(self, lufs_mode):
+        return np.max(np.abs(self.get_data_for_mode(lufs_mode))) >= 1.0
+
+    def get_data(self, start, end, lufs_mode):
+        return self.get_data_for_mode(lufs_mode)[start:end, :]
+
+    def get_data_for_mode(self, lufs_mode):
         match lufs_mode:
             case "raw":
-                self.currently_playing = self.data
+                return self.data
             case "mixing":
-                if quietest != self.mixing_lufs:
-                    self.mixing_data = self.normalize_data(quietest)
-                    self.mixing_lufs = quietest
-                self.currently_playing = self.mixing_data
+                return self.mixing_data
             case "mastering":
-                self.currently_playing = self.mastering_data
-
-        to_play = self.currently_playing[start:end, :]
-        return to_play
+                return self.mastering_data
 
     def fade_in(self, data):
         fade_len = 20
